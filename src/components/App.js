be6937282615b8b2fff33975fbbb26b76ea23fea
Cassi Gallagher <ccgall@gmail.com>
2017-08-12 13:11:44 -0700

    Create basic navigation

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,16 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <div className="App-logo" alt="logo">CGS</div>
-          <nav>Welcome to React</nav>
+        <div className="App-logo" alt="logo">CGS</div>
+
+            <ul className='App-nav'>
+              <li>About</li>
+              <li>Work</li>
+              <li>Ongoing Projects</li>
+              <li>About Me</li>
+              <li>Contact</li>
+              <li>Blog</li>
+            </ul>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
