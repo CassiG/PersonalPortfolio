e5167d079bdaf2f10ef21a44d18347d2289b4565
Cassi Gallagher <ccgall@gmail.com>
2017-08-28 13:40:13 -0700

    Update color scheme and fonts

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Home extends Component {
   render() {
     return(
-      <div>
+      <div className='home-style'>
         <h1>Hello!</h1>
         <p>I’m a full-stack web developer based out of Seattle, WA. WIth a background in journalism and design I have an editor’s eye that keeps me grounded with clean and concise code, and a design sense that will help me easily visualize the whole picture of any problem I’m trying to solve. </p>
       </div>
