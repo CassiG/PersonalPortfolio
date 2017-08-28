5da9772c4cebfb2e0c63fd9ed4be5dfee8d6e6c0
Cassi Gallagher <ccgall@gmail.com>
2017-08-28 13:47:50 -0700

    Update homepage with link to contact and update content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
   render() {
     return(
       <div className='home-style'>
         <h1>Hello!</h1>
-        <p>I’m a full-stack web developer based out of Seattle, WA. WIth a background in journalism and design I have an editor’s eye that keeps me grounded with clean and concise code, and a design sense that will help me easily visualize the whole picture of any problem I’m trying to solve. </p>
+        <p>I’m a full-stack web developer based out of Seattle, WA. With an additional background in journalism and design I have an editor’s eye that keeps me grounded with clean and concise code, and a design sense that will help me easily visualize the whole picture of any problem I’m trying to solve. </p>
+        <button><Link to='/contact'>Let's chat</Link></button>
       </div>
     );
   }
