201dcd95ff163d25bdac19f0411737450ae2fddd
Cassi Gallagher <ccgall@gmail.com>
2017-08-28 16:44:26 -0700

    Add contact form using formspring

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 
 class Contact extends Component {
+
+  submitForm(e) {
+    e.preventDefault();
+    $.ajax({
+      url: "https://formspree.io/ccgall@gmail.com",
+      method: "POST",
+      data: $('.contact-form').serialize(),
+      dataType: "json"
+    });
+  }
+
   render() {
     return(
       <div>
         <h1>Let's Chat</h1>
-        <p className='contact-description'>Want to chat more about web development, video games, or how silly your dog is? Let's chat more. Email me at ccgall@gmail.com or send me a message here! Look forward to talking more!</p>
-        <form action="mailto:ccgall@gmail.com">
-          <input type="text" className="input-lg" placeholder='Name'></input>
-          <input type="email" className="input-lg" placeholder='Email'></input>
-          <textarea className="input-lg" placeholder="Message"></textarea>
-          <button type="button" className="button">Submit</button>
+        <p className='contact-description'>Want to chat more about web development, video games, or how silly your dog is? Let's chat more. <br /><br />Email me at ccgall [at] gmail [dot] com or send me a message here! Look forward to talking more!</p>
+        <form action="https://formspree.io/ccgall@gmail.com" method="POST">
+            <input type="text" name="name" className="input-lg" placeholder='Your Name'></input>
+            <input type="email" name="_replyto" className="input-lg" placeholder='Your Email'></input>
+            <textarea name="message" placeholder="Your awesome message"></textarea>
+            <input type="submit" value="Send"></input>
         </form>
       </div>
 
