a069293a7d26236ab74748e05aac2caa5cceebb3
Cassi Gallagher <ccgall@gmail.com>
2017-08-28 18:20:46 -0700

    Create mobile navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,67 @@ import { Link } from 'react-router-dom';
 import '.././css/Header.css';
 
 class Header extends Component {
-  constructor() {
-    super();
-    this.clickHandler = this.clickHandler.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      windowWidth: window.innerWidth,
+      mobileNavVisable: false
+    };
   };
 
-  clickHandler(e) {
-    this.props.setHeader(e.target.href)
+  handleResize() {
+    this.setState({ windowWidth: window.innerWidth });
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize.bind(this));
+  };
+
+  componentWillUnmout() {
+    window.removeEventListener('resize', this.handleResize.bind(this));
+  }
+
+  navigationLinks() {
+    return [
+      <ul className='Header-nav'>
+        <li key={1}><Link to = '/about'>About</Link></li>
+        <li key={2}><Link to = '/projects'>Projects</Link></li>
+        <li key={3}><Link to='/contact'>Contact</Link></li>
+      </ul>
+    ];
+  }
+
+  renderMobileNav() {
+    if(this.state.mobileNavVisable){
+      return this.navigationLinks();
+    }
+  }
+
+  renderNavigation() {
+    if(this.state.windowWidth <= 1080) {
+      return [
+        <div className="mobile-nav">
+          <p onClick={this.handleNavClick.bind(this)}>
+            Menu
+          </p>
+          {this.renderMobileNav()}
+        </div>
+      ];
+    } else {
+      return [
+        <div key={7} className='nav-menu'>
+          {this.navigationLinks()}
+        </div>
+      ];
+    }
+  }
+
+  handleNavClick() {
+    if(!this.state.mobileNavVisable) {
+      this.setState({mobileNavVisable: true});
+    } else {
+      this.setState({mobileNavVisable: false});
+    }
   }
 
   render() {
@@ -18,15 +72,9 @@ class Header extends Component {
       <div>
         <div className="Header">
         <div className="Header-logo" alt="logo">
-          <Link to ='/' onClick={this.clickHandler}>CGS</Link>
+          <Link to ='/'>CGS</Link>
         </div>
-          <nav id="nav">
-            <ul className='Header-nav'>
-              <li><Link to = '/about' onClick={this.clickHandler}>About</Link></li>
-              <li><Link to = '/projects' onClick={this.clickHandler}>Projects</Link></li>
-              <li><Link to='/contact' onClick={this.clickHandler}>Contact</Link></li>
-            </ul>
-          </nav>
+          {this.renderNavigation()}
         </div>
       </div>
     )
