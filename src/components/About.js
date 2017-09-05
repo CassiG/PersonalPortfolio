5ff41920128166d0c0c379ace11063b7f6b55779
Cassi Gallagher <ccgall@gmail.com>
2017-09-05 14:44:49 -0700

    Update about page

--- src/components/About.js
+++ src/components/About.js
@@ -7,11 +7,9 @@ class About extends Component {
       <div className="about-me">
         <h1>About</h1>
         <img src="cassigallagher.jpg" alt="Cassi Gallagher-Sherer" />
-        <p>I'm Cassi - a full stack web developer and nerd-of-all-trades based of Seattle, WA. I’ve always had two big passions: Problem solving and visual communication. </p>
+        <p>I'm a full-stack web developer based in Seattle, WA. I'm currently seeking new opportunities in the Front-End Development space.</p>
 
-        <p>The latter first led me to visual journalism, where I learned to have a design eye as well as write and edit. However, problem-solving and code were something I found myself frequently coming back to - from taking a web development certificate in college, to teaching myself to code, to eventually going a fully immersive full-stack development program, Dev Bootcamp, I loved the idea of getting to build things through code.</p>
-
-        <p>Now as a developer, I love being able to combine these two passions. Not only getting to problem solve through code, having a design eye helps me better visualize a problem and also see where a project is going, being an editor gives me a keen eye for detail and is at the forefront of my brain as I code, making me always think about how I can write clean and concise code. </p>
+        <p>I'm passionate about creative problem solving and learning new things. I particularly love visual communication and it's role in shaping our understanding of a topic or captivating an audience. With a B.A. in Visual Journalism from <a href ="https://www.wwu.edu/" target="_blank">Western Washington University</a> and a Full-Stack Web Development Certificate from <a href="https://devbootcamp.com/" target="blank_">Dev Bootcamp</a>, so my coding super power is an editor's eye that can catch bug and keep code consise and clean. I love learning new things and currently you can probably find me leveling up my code at HackerRank or Frontend Masters, or attending a Women Who Code Meetup or Hackathon.</p> 
 
         <p>In my free time, you might catch me playing video games (especially if they've got beautiful art or a killer soundtrack - Firewatch and Last of Us come to mind), trying out a new brewery with <a href="http://www.jeffrshearer.com/" target="_blank">my husband </a>, volunteering with <a href ="http://www.theworldisfun.org/" target="_blank">The World is Fun</a>, or <a href ="https://www.instagram.com/leianotorgana/" target="_blank">hanging out with my dog.</a></p>
 
