498b5f42043c2c688ca73012b492f13f7a978e28
Cassi Gallagher <ccgall@gmail.com>
2017-09-08 12:18:10 -0700

    Add markdown converter for contentful content

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/index';
+import MDReactComponent from 'markdown-react-js';
 
 class Blog extends Component {
   componentWillMount() {
     this.props.fetchPosts();
   }
+
   renderPosts() {
     return this.props.posts.map((post, index) => {
       return (
         <article key={post.sys.id}>
           <h3>{post.fields.title}</h3>
-          <p>{post.fields.body}</p>
+          <MDReactComponent text={post.fields.body} />
         </article>
       );
     });
