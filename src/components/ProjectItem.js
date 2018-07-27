23052003d40df7bc5113822529d1ee394f90ae92
CassiG <ccgall@gmail.com>
2018-07-27 09:44:19 -0700

    Fix project links

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -13,8 +13,8 @@ class ProjectItem extends Component {
           <p>About the Project: {project.longDescription}</p>
           <p>Front-End Technology Used: {project.frontEnd}</p>
           <p>Back-End Technology Used: {project.backEnd}</p>
-          <button><a href={project.website} target="_blank" rel="noopener noreferrer">View Live</a></button>
-          <button><a href={project.github} target="_blank" rel="noopener noreferrer">See Source</a></button>
+          <a href={project.website} target="_blank" rel="noopener noreferrer"><button>View Live</button></a>
+          <a href={project.github} target="_blank" rel="noopener noreferrer"><button>See Source</button></a>
         </div>
       </div>
     );
