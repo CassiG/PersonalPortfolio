b23f8a9757feeda1e84200be987baedf3bebe5a5
CassiG <ccgall@gmail.com>
2018-08-30 15:25:47 -0700

    Add link to individual blog page and render basic blog data

--- src/components/Blog/BlogItem.js
+++ src/components/Blog/BlogItem.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 class BlogItem extends Component {
     render(){
         return(
-            <div></div>
+            <div>
+                <Link to={{
+                    pathname: `/blog${this.props.path}`,
+                    state: { ...this.props }
+                }} >{this.props.title}</Link>
+            </div>
         )
     }
 }
