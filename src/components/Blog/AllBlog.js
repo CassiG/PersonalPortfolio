0f5cc029d55a1969d54a1499cab6d2f9ccd452da
CassiG <ccgall@gmail.com>
2018-08-30 15:20:40 -0700

    fixed render post function to collect the right data

--- src/components/Blog/AllBlog.js
+++ src/components/Blog/AllBlog.js
@@ -3,40 +3,42 @@ import * as contentful from 'contentful';
 import BlogItem from './BlogItem';
 
 class AllBlog extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            posts: []
-        }
-        this.contentfulClient = contentful.createClient({
-            space: process.env.REACT_APP_SPACE_KEY,
-            accessToken: process.env.REACT_APP_ACCESSTOKEN
-        })
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+    this.contentfulClient = contentful.createClient({
+      space: process.env.REACT_APP_SPACE_KEY,
+      accessToken: process.env.REACT_APP_ACCESSTOKEN
+    });
+  }
 
-    componentDidMount(){
-        this.fetchPosts().then(this.postsToState)
-    }
+  componentDidMount() {
+    this.fetchPosts().then(this.postsToState);
+  }
 
-    fetchPosts = () => this.contentfulClient.getEntries();
-    postsToState = response => {
-       this.setState({
-           posts: response.items
-       })
-    }
+  fetchPosts = () => this.contentfulClient.getEntries();
+  postsToState = response => {
+    this.setState({
+      posts: response.items
+    });
+  };
 
-    renderPosts = () => {
-        return this.state.posts.map( ({posts}, id) => <BlogItem key={id} {...posts} />)
-    }
+  renderPosts = () => {
+    return this.state.posts.map(({ fields }, id) => (
+      <BlogItem key={id} {...fields} />
+    ));
+  };
 
-    render(){
-        return(
-            <div>
-                <h1>Blog</h1>
-                {this.renderPosts()}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Blog</h1>
+        {this.renderPosts()}
+      </div>
+    );
+  }
 }
 
 export default AllBlog;
