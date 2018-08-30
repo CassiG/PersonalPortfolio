2db26b2904e8e85fb88b173014ba5449a5710f67
CassiG <ccgall@gmail.com>
2018-08-30 11:07:57 -0700

    Remove redux for contentful posts

--- src/index.js
+++ src/index.js
@@ -4,21 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import reducers from './reducers';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
 
-
-import promise from 'redux-promise';
-
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render((
-  <Provider store={createStoreWithMiddleware(reducers)}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  </Provider>
 ), document.getElementById('root'));
 
 registerServiceWorker();
