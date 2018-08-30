84605ef19a17efa6acb6214f51cad206c8488e22
CassiG <ccgall@gmail.com>
2018-08-30 15:17:11 -0700

    Fetch contentful API and create component to render posts

---
+++ src/components/Blog/AllBlog.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import * as contentful from 'contentful';
+import BlogItem from './BlogItem';
+
+class AllBlog extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            posts: []
+        }
+        this.contentfulClient = contentful.createClient({
+            space: process.env.REACT_APP_SPACE_KEY,
+            accessToken: process.env.REACT_APP_ACCESSTOKEN
+        })
+    }
+
+    componentDidMount(){
+        this.fetchPosts().then(this.postsToState)
+    }
+
+    fetchPosts = () => this.contentfulClient.getEntries();
+    postsToState = response => {
+       this.setState({
+           posts: response.items
+       })
+    }
+
+    renderPosts = () => {
+        return this.state.posts.map( ({posts}, id) => <BlogItem key={id} {...posts} />)
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Blog</h1>
+                {this.renderPosts()}
+            </div>
+        )
+    }
+}
+
+export default AllBlog;
---
+++ src/components/Blog/BlogItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class BlogItem extends Component {
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default BlogItem;
